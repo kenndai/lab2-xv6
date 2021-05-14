11bd33a3f78903346ec01c1222538235ec0fbbbb
kenndai <heyitskennydai@gmail.com>
2021-05-13 18:53:00 -0700

    Added the test files

---
+++ prog1.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+//burst time should be similar throughout all three processes
+
+int main(int argc, char* argv[]) {
+    set_priority(1);
+    int i, k;
+    for (i = 0; i < 43000; i++) {
+        asm("nop");
+        for (k = 0; k < 43000; k++) {
+            asm("nop");
+        }
+    }
+    printf(1, "prog1: ");
+    exit(); //will print out turnaroundTime, burstTime, and waiting time
+}
